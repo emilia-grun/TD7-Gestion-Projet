5805feeae0b3fed91d785a901e95d46fb6ced0b0
lucrese <bebey-dooh.lucresse-desiree-grace@epitech.eu>
2026-03-16 17:29:29 +0100

    Add: Création de la fonction minCh qui prends en compte avec NULL le min de 2 chaines

---
+++ comparaison.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+#include "projet.h"
+
+char *minCh(char *c1, char *c2)
+{ // Le minimum de 2 chaînes avec prise en compte de NULL
+    if (c2 == NULL)
+        return c1;
+    else if (c1 == NULL || strcmp(c1, c2) > 0)
+        return c2;
+
+    return c1;
+    // Si les deux chaînes sont identiques, alors c1 sera renvoyé par défaut
+}
