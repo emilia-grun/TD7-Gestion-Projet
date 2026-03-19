824cc3a88a76e71d424be1b2d372de41234620df
Emilia <emilia.grun@outlook.com>
2026-03-19 14:36:10 +0100

    Ajoute l'option de valeur minimale et maximale

--- main.c
+++ main.c
@@ -27,32 +27,32 @@ int main(int argc, char **argv)
     {
         printf("\n--- Menu ---\n");
         printf("1. Ajouter un element\n2. Supprimer un element\n3. Taille de l'arbre\n");
-        printf("4. Hauteur de l'arbre\n5. Afficher l'arbre\n-1. Quitter\n> ");
+        printf("4. Hauteur de l'arbre\n5. Afficher l'arbre\n6. Valeur minimale et maximale\n-1. Quitter\n> ");
 
         if (scanf("%d%*c", &action) != 1)
             break;
 
         switch (action)
         {
-        case 1: //Ajout
+        case 1: // Ajout
             printf("Chaine a ajouter : ");
             fgets(buffer, sizeof(buffer), stdin);
             buffer[strcspn(buffer, "\n")] = 0;
             racine = insertionEq(racine, buffer);
             break;
-        case 2: //Suppression
+        case 2: // Suppression
             printf("Chaine a supprimer : ");
             fgets(buffer, sizeof(buffer), stdin);
             buffer[strcspn(buffer, "\n")] = 0;
             racine = suppression(racine, buffer);
             break;
-        case 3: //Taille
+        case 3: // Taille
             printf("Nombre d'éléments : %d\n", taille(racine));
             break;
-        case 4: //Hauteur
+        case 4: // Hauteur
             printf("Hauteur de l'arbre : %d\n", hauteur(racine));
             break;
-        case 5: //Affichage
+        case 5: // Affichage
             printf("-- Affichage --\n1. Ordre préfixe\n2. Ordre infixe\n3. Ordre suffixe\n> ");
             scanf("%d%*c", &action_affichage);
             switch (action_affichage)
@@ -71,7 +71,11 @@ int main(int argc, char **argv)
                 continue;
             }
             break;
-        case -1:
+        case 6: // Valeur min et max
+            printf("La chaîne minimale de l'arbre est : %s\n", minimum(racine));
+            printf("La chaîne maximale de l'arbre est : %s\n", maximum(racine));
+            break;
+        case -1: // Arrêt
             printf("Au revoir !\n");
             break;
         default:
